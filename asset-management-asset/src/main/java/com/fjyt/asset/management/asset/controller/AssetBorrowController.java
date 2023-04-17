2ef3cf7e88f304eb20fc42505c2e6221691f1891
kql <769156974@qq.com>
2023-04-17 15:10:41 +0800

    资产模块创建

---
+++ asset-management-asset/src/main/java/com/fjyt/asset/management/asset/controller/AssetBorrowController.java
@@ -0,0 +1,13 @@
+package com.fjyt.asset.management.asset.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author keQiLong
+ * @date 2023年04月17日 15:03
+ */
+@RestController
+@RequestMapping("/assetBorrow")
+public class AssetBorrowController {
+}
---
+++ asset-management-asset/src/main/java/com/fjyt/asset/management/asset/controller/AssetMaintenanceController.java
@@ -0,0 +1,14 @@
+package com.fjyt.asset.management.asset.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author keQiLong
+ * @date 2023年04月17日 15:05
+ * 资产维修Controller
+ */
+@RestController
+@RequestMapping("/assetMaintenance")
+public class AssetMaintenanceController {
+}
